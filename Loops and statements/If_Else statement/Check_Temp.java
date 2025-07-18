5b2c2983a8840b1aab6b2376d564575383d943ba
kiran-more1104 <kiransmore11204@gmail.com>
2025-07-18 17:36:03 +0530

    Add files via upload

---
+++ Loops and statements/If_Else statement/Check_Temp.java
@@ -0,0 +1,13 @@
+public class Check_Temp {
+    public static void main(String[] args) {
+        int temp = 22;
+
+        if (temp > 30) {
+            System.out.println("Hot");
+        } else if (temp >= 20) {
+            System.out.println("Warm");
+        } else {
+            System.out.println("Cold");
+        }
+    }
+}
